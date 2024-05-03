245360377ec0bc48e93e363a8e05654e9945946e
Blanca Tixi <blanca@Blancas-MBP.lan>
2024-05-03 01:24:56 -0400

    Created the APIAssetsManager Class

---
+++ Determit/Determit/Determit/Services/AssetsCoreDataManager.swift
@@ -0,0 +1,35 @@
+//
+//  CoreDataManagerAssets.swift
+//  Determit
+//
+//  Created by Blanca Tixi on 5/2/24.
+//
+
+import Foundation
+import CoreData
+
+
+class AssetsCoreDataManager{
+    
+    
+    
+    //MARK: - ASSETS METHODS
+    
+    
+    //CREATE
+    
+    func CreteAsset( _ device:APIDevice){
+        
+        
+    }
+    
+    
+    
+    //READ_ALL
+    //READ_BY_ID
+    //UPDTE
+    //DELETE
+    
+    
+    
+}
