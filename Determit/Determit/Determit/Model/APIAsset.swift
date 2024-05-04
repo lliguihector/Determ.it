0c87cdf5fab4e5054429c00381cff38abf43fdb7
Blanca Tixi <blanca@Blancas-MacBook-Pro.local>
2024-05-03 20:25:49 -0400

    Created Class Files APIAsset and AssetCoreDataManger

---
+++ Determit/Determit/Determit/Model/APIAsset.swift
@@ -0,0 +1,18 @@
+//
+//  APIAsset.swift
+//  Determit
+//
+//  Created by Blanca Tixi on 5/3/24.
+//
+
+import Foundation
+
+struct APIAsset: Codable{
+    
+    
+    
+    let assetTag: String
+    let devicdeID: String
+    let serialNumber: String
+    
+}
